643f28de0c40836703b9d054c5a8770cc37e8c56
Dan Vera <arubinofaux@gmail.com>
2020-06-07 02:56:00 -0400

    wip

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 source 'https://rubygems.org'
 
 gem 'httparty'
-gem 'sys-cpu'
-gem 'sys-filesystem'
--- src/agent.rb
+++ src/agent.rb
@@ -1,8 +1,6 @@
 require 'yaml'
 require 'json'
 require 'httparty'
-require "sys/cpu"
-require 'sys/filesystem'
 require "pp"
 
 CONF = YAML.load_file("/var/deploth-agent/agent.yml")
@@ -28,31 +26,24 @@ module Agent
 
   class Client
     def self.status
-      cpu = Sys::CPU.load_avg.last
-
       getMemory = %x(free)
       totalMemory = getMemory.split(" ")[7]
       usedMemory = getMemory.split(" ")[8]
       freeMemory = getMemory.split(" ")[9]
 
-      dir = Sys::Filesystem.stat("/")
-      diskAvailable = dir.block_size * dir.blocks_available / 1024 / 1024
+      cpuLoad = %x(w | head -1).strip.split(":").last.split(",").first.strip
 
-      dir = Sys::Filesystem.stat("/")
-      diskTotal = (dir.blocks * dir.block_size).to_mb
-      diskAvailable = (dir.blocks_available * dir.block_size).to_mb
-      diskUsed = 100 - (100.0 * diskAvailable.to_f / diskTotal.to_f)
+      diskUsage = `df -m /`.split(/\b/)[26]
 
+      data = {
+        cpu_load: cpuLoad,
+        disk_usage: diskUsage,
+        memory_total: totalMemory,
+        memory_used: usedMemory,
+        memory_free: freeMemory
+      }
 
-      pp cpu
-
-      pp totalMemory
-      pp usedMemory
-      pp freeMemory
-
-      pp diskTotal
-      pp diskAvailable
-      pp diskUsed
+      Helper.ping(data)
     end
   end
 end
